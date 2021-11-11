b7396dd3934b617262200bb41b43c8c5e175b3d0
khalidq812 <mohammad.ali.github@gmail.com>
2021-11-11 20:10:38 +0500

    Implemented task 4
    Added the functionality to process file line by line to create dictionary and append it to the sales_data list

--- converter.py
+++ converter.py
@@ -7,3 +7,28 @@ sales_data = []
 # 2. Opening the input file
 input_file = open(input_file_name, 'r')
 
+# 3. Processing the file line by line
+
+# For each line
+for line in input_file:
+    # Strip the line for new lines and split it on comma
+    line = line.strip()
+    attributes = line.split(',')
+    
+    dictionary = {} # Inner dictionary
+    previous_attribute = None # Previous attribute or dictionary reference
+    
+    # For each attribute in reversed iteration
+    for attribute in reversed(attributes):
+        
+        dictionary = {}     # Empty the dictionary
+        # If it's the last attribute, just update the previous attribute
+        if attribute == attributes[-1]:
+            previous_attribute = attribute
+        else:
+            # Else just add the current attribute as key for value of previous attribute
+            dictionary[attribute] = previous_attribute
+            previous_attribute = dict(dictionary)
+
+    # Append the dictionary into list
+    sales_data.append(dictionary)
